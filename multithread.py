3b03328a92f3c46bfea590004b6dc0f34f766239
deutranium <29124655+deutranium@users.noreply.github.com>
2023-04-24 21:15:42 +0530

    Added multithread script

---
+++ multithread.py
@@ -0,0 +1,33 @@
+import concurrent.futures
+import pickle
+
+NUM_WORKERS = 4
+
+
+pool = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+img_arr = [[3], [5], [6], [2]]
+flat = [item for sublist in img_arr for item in sublist]
+data = {i: {"X": [], "Y": []} for i in flat}
+
+count = 0
+
+def compute_xy(i):
+    return f"X_{i}", f"Y_{i}"
+
+def run_code(arr):
+    global count
+    for img in arr:
+        X, Y = compute_xy(img)
+        print("count", count)
+        data[img]["X"] = X
+        data[img]["Y"] = Y
+        count += 1
+
+for i in range(NUM_WORKERS):
+    pool.submit(run_code, img_arr[i])
+
+pool.shutdown(wait=True)
+print("computation done..")
+
+print("Writing to pickle...")
+pickle.dump(data, open("save_xy.p", "wb"))
